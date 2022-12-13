a065d1b4c5df254f735b9049c1746696386ed4b4
Luc Labarrière <luc.labarriere@live.fr>
2022-12-13 16:19:18 +0100

    Indication at the end of the application

--- OrbitalEditor/src/main.cpp
+++ OrbitalEditor/src/main.cpp
@@ -2,11 +2,14 @@
 
 using namespace Orbital;
 
+
 int main(int argc, char** argv)
 {
 	auto app = MakeRef<EditorApplication>();
 	app->run(argc, argv);
 	app.reset();
-	Logger::Log("Done");
+
+	LOGFUNC();
+	Logger::Log("return 0;");
 	return 0;
 }
